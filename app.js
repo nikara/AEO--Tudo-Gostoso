bea5356ca7d97b722c7d269ae0a96dadf7aac45a
nikara <nickolaslamin@gmail.com>
2025-10-29 20:03:13 -0300

    Refactor database table creation and update package.json to use ES modules

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-import {"sqlite3"} from 'sqlite3';
-import {open} from 'sqlite';
+import sqlite3 from "sqlite3";
+import {open} from "sqlite";
 
 async function criarEPopularTabelaUsuario(nome,sobrenome){
     const db = await open({
-        filename:'./banco.db',
-        driver: sqlite3.driver,
+        filename:"./banco.db",
+        driver: sqlite3.Database,
     });
 
-    db.run(`CREATE TABLE IF NOT EXIST usuarios (id INTEGER PRIMARY KEY, nome TEXT, sobrenome TEXT)`);
-    db.run(`INSERT INTO usuarios (nome,sobrenome) VALUES (?,?)`,[nome,sobrenome,]);
+    db.run(`CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY, nome TEXT, sobrenome TEXT)`);
+    
+    
+
 }
 
 criarEPopularTabelaUsuario("Daniel","Porto");
+
+    //db.run(`INSERT INTO usuarios (nome,sobrenome) VALUES (?,?)`,[nome,sobrenome,]);
