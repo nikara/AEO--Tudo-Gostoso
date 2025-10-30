65ed0c83e11a7fe8202c88aacc4ae3549afcbbb0
nikara <nickolaslamin@gmail.com>
2025-10-30 17:40:07 -0300

    atualização

---
+++ server.js
@@ -0,0 +1,17 @@
+require ('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const PORT  = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/',(req,res) => {
+    res.send('Servidor Node.js + Express rodando com sucesso!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
