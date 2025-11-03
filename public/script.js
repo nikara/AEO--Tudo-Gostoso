18b2e8e5e51beb54424b65504578a952a8ef0241
nikara <nickolaslamin@gmail.com>
2025-11-03 19:37:53 -0300

    atualizar

---
+++ public/script.js
@@ -0,0 +1,81 @@
+const API_URL = '/api/tarefas';
+
+async function buscarTarefas() {
+    try{
+        const response = await fetch(API_URL);
+        const tarefas = await response.json();
+        renderizarTarefas(tarefas);
+    }catch(error){
+        console.error('Erro ao buscar tarefas:', error);
+    }
+}
+
+function renderizarTarefas(tarefas){
+    const lista = document.getElementById('tarefas-lista');
+    lista.innerHTML = '';
+
+    tarefas.forEach(tarefa => {
+        const item = document.createElement('li');
+        item.className = 'tarefa-item';
+        if(tarefa.concluida){
+                item.classList.add('concluida');
+        }
+
+        item.innerHTML = '<span>' + tarefa.titulo + '</span>' + 
+            '<div> ' +
+                '<button onclick="toggleConcluida('+ tarefa.id + ','+ tarefa.concluida +')">' + (tarefa.concluida ? 'Desfazer' : 'Concluir') + '</button>' +
+                '<button onclick="deletarTarefa('+tarefa.id+')">Excluir</button>'+
+            '</div';
+        lista.appendChild(item);    
+    });
+}
+
+async function adicionarTarefa() {
+    const input = document.getElementById('nova-tarefa-titulo');
+    const titulo = input.value.trim();
+
+    if(!titulo) return;
+
+    try{
+        await fetch(API_URL,{
+            method: 'POST',
+            headers:{
+                'Content-type': 'application/json'
+            },
+            body:JSON.stringify({titulo:titulo})
+        });
+
+        input.value = '';
+        buscarTarefas();
+    }catch(error){
+    console.error('Erro ao adicionar tarefa:', error);
+    }
+    
+}
+
+async function toggleConcluida(id,concluidaAtual) {
+    try{
+        await fetch(API_URL + '/' + id,{
+            method: 'PUT',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({concluida: !concluidaAtual})
+        });
+    }catch(error){
+        console.error('Erro ao atualizar tarefa:',error);
+    }
+}
+
+async function deletarTarefa(id) {
+    try{
+        await fetch(API_URL + '/' + id,{
+            method:'DELETE'
+        });
+        buscarTarefas();
+    }catch(error){
+        console.error('Erro ao deletar tarefa:', error);
+    }
+}
+
+document.addEventListener('DOMContentLoaded',buscarTarefas);
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 require ('dotenv').config();
 
 const express = require('express');
+const path = require('path');
 const app = express();
 
 require('./src/config/db');
 
+const tarefaRoutes = require('./src/routes/tarefaRoutes');
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/api/tarefas',tarefaRoutes);
 
 app.get('/',(req,res) => {
     res.send('Servidor Node.js + Express rodando com sucesso!');
