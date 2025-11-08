bd1f3b74de72e5e0825736ad06ec750153febf69
Nikara <nickolaslamin@gmail.com>
2025-11-08 19:51:59 -0300

    Atualiza configuração do banco de dados

--- src/config/db.js
+++ src/config/db.js
@@ -9,7 +9,13 @@ function initDB(){
     const sql = `CREATE TABLE IF NOT EXISTS tarefas(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     titulo TEXT NOT NULL,
-    concluida INTEGER DEFAULT 0)`
+    concluida INTEGER DEFAULT 0);
+    
+    CREATE TABLE IF NOT EXIST receita(
+    idreceita INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT NOT NULL,
+    descricao TEXT,
+    imagem TEXT `
 
     db.exec(sql);
     console.log('Banco de dados SQLite inicializado e tabela "tarefas" verificada.');
